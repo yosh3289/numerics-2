5d7ea9b236e1f1fd3cda80e7af4f949f77cddcc8
davisconklin <davis.conklin@gmail.com>
2017-04-14 22:48:46 -0600

    Added better comments, changed Qinf to global variable that must be defined outside normal.m

--- code/normal.m
+++ code/normal.m
@@ -1,9 +1,21 @@
+% Normal model for oil production rates
+% x - values to evaluate the model at
+% a - mean
+% b - standard deviation
+%
+% Note: Qinf must be defined elsewhere before calling this function!
+
 function [y,ya,yb] = normal(x,a,b)
 
-Qinf = 2e6;
+global Qinf
 
+% model prediction
 y = (Qinf/b/sqrt(2*pi))*exp((-1/2).*((x-a)./b).^2);
+
+% partial derivative of model wrt a
 ya = (Qinf.*(x-a)./b^3./sqrt(2*pi)).*exp((-1/2).*((x-a)./b).^2);
+
+% partial derivative of model wrt b
 yb = (Qinf.*((x-a).^2-b^2)./b^4./sqrt(2*pi)).*exp((-1/2).*((x-a)./b).^2);
 
 end
