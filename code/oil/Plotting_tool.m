de58578694e766790cdfd2c0fecfb2425eac9758
davisconklin <davis.conklin@gmail.com>
2017-04-16 19:54:09 -0600

    -reorganized files -improved plotting utility -new plot

--- code/oil/Plotting_tool.m
+++ code/oil/Plotting_tool.m
@@ -3,21 +3,33 @@ global Qinf
 x=OilProductionData(:,1);
 y=OilProductionData(:,2);
 
-xpr=1850:2200;
+xpr=1850:2150;
 Qinf = 2e6;
 [a1,b1]=Fitter(OilProductionData,@normal,2000,40);
 ypr1 = normal(xpr,a1,b1);
+ym1 = normal(a1,a1,b1);
+x01 = 2075.39;
+y01 = 0.05*ym1;
 Qinf = 3e6;
 [a2,b2]=Fitter(OilProductionData,@normal,2000,40);
 ypr2 = normal(xpr,a2,b2);
+ym2 = normal(a2,a2,b2);
+x02 = 2112.02;
+y02 = 0.05*ym2;
 Qinf = 4e6;
 [a3,b3]=Fitter(OilProductionData,@normal,2000,40);
 ypr3 = normal(xpr,a3,b3);
+ym3 = normal(a3,a3,b3);
+x03 = 2138.85;
+y03 = 0.05*ym3;
 
-plot(x,y,'k.',xpr,ypr1,'r--',xpr,ypr2,'g--',xpr,ypr3,'b--')
+
+plot(x,y,'k.',xpr,ypr1,'r--',xpr,ypr2,'g--',xpr,ypr3,'b--',...
+    [x01,x02,x03],[y01,y02,y03],'m.',...
+    [a1,a2,a3],[ym1,ym2,ym3],'c.')
 
 legend('Data','Q_{\infty}= 2 tn barrels','Q_{\infty}= 3 tn barrels',...
-    'Q_{\infty}= 4 tn barrels','Location','NorthWest');
+    'Q_{\infty}= 4 tn barrels','Oil Depletion','Maximum Production','Location','NorthWest');
 xlabel('Year');
 ylabel('Oil Production Rate (millions of barrels/year)')
 title('Oil Production Rate: Normal Distribution Fit');
